f06bbd1bb74a0daacd886994077273a0c3e10095
Mawoka <mawoka@e.email>
2021-10-04 17:31:13 +0200

    Started index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,21 @@
-<div class="pt-14">
-    <h1>Hallo</h1>
+<script>
+	import 'atropos/css';
+	import Atropos from 'atropos';
+	import JustBackground from '../assets/JustBackground.png';
+	import Logo from '../assets/LogoNoBG.png';
+</script>
+
+<div class="pt-16 lg:pt-24">
+
+	<!-- <div class="atropos my-atropos" id=".my-atropos">
+		<div class="atropos-scale">
+			<div class="atropos-rotate">
+				<div class="atropos-inner">
+					<img src={JustBackground} data-atropos-offset="-5" />
+					<img src={Logo} data-atropos-offset="05" />
+				</div>
+			</div>
+		</div>
+	</div> -->
+    <h1 class="text-center text-3xl">Millencium</h1>
 </div>
